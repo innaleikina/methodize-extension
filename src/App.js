3e36d6860b840e0be874c3fc6fecc3f5604435d9
Inna <designbyinna@gmail.com>
2019-09-03 21:48:57 -0600

    chrome extension code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      WELCOME TO METHODIZE
+    </div>
+  );
+}
+
+export default App;
